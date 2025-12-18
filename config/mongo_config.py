8f400ab1824f451cb1350abca52c6d48c940b96b
prudvi-raj-git <prudvi.t6@gmail.com>
2025-12-18 18:31:12 +0530

    logs and exception added

---
+++ config/mongo_config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# Load values from .env file (optional but recommended)
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME", "VehInsDomApp")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "VehInsDomApp-Data")
+CONNECTION_URI = os.getenv("MONGODB_URI")
